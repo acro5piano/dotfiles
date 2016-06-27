4df687b538837efc05deab31948da4e1bf7b58b8
gosho-kazuya <ketsume0211@gmail.com>
2016-06-27 19:44:08 +0900

    add config files

---
+++ .config/autostart/touch.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sleep 6
+synclient TapButton2=3
+#synclient TapButton3=0
+#synclient ClickFinger2=0
+#synclient ClickFinger3=0
+#synclient HorizTwoFingerScroll=0
+#synclient VertTwoFingerScroll=0
+touchegg
+
