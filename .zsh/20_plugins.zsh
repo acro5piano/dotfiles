e407220536a3bc8ecbedbd5cac47c1418f5927dc
gosho-kazuya <ketsume0211@gmail.com>
2016-06-29 01:32:12 +0900

    ALL Update

---
+++ .zsh/20_plugins.zsh
@@ -0,0 +1,10 @@
+antigen use oh-my-zsh
+
+# Syntax highlighting bundle.
+antigen bundle zsh-users/zsh-syntax-highlighting
+
+# Load the theme.
+antigen theme robbyrussell
+
+# Tell antigen that you're done.
+antigen apply
