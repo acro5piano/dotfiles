915e1b91fdd7785dce0337cec108b53145e4c267
gosho-kazuya <ketsume0211@gmail.com>
2016-07-08 18:38:18 +0900

    do not rely on hostname

--- .profile.d/20_other.sh
+++ .profile.d/20_other.sh
@@ -33,7 +33,7 @@ if [ $USER = 'kazuya' ]; then
 fi
 
 # Setting for my Office
-if [ `hostname` = 'kgosho-ubuntu' ]; then
+if [ -e "~/.quick_dotfiles" ]; then
     . ~/.quick_dotfiles/.bash_profile
     . ~/.quick_dotfiles/.bash_aliases
 fi
