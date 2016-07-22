8f8278944214b635a02a7b2d0a3ca14bbfc6165e
gosho-kazuya <ketsume0211@gmail.com>
2016-07-23 02:32:12 +0900

    add some useful tools

--- .profile.d/10_aliases.sh
+++ .profile.d/10_aliases.sh
@@ -1,11 +1,10 @@
 # Define my commands alias or function
 
-alias P='cat /tmp/clipboard'
+
 alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
 alias cl='xsel -ib'
 alias clp='xsel -ob'
 alias egrep='egrep --color=auto'
-#alias em='emacs -nw'
 alias em='emacs'
 alias fgrep='fgrep --color=auto'
 alias gita='git add -A . ; git commit -m "ALL Update" ; git push'
@@ -13,24 +12,22 @@ alias grep='grep --color=auto'
 alias l='ls -CF'
 alias la='ls -A'
 alias less='less -R'
-alias ll='ls -alF'
+alias ll='ls -al'
 alias ls='ls --color=auto'
 alias memo='em ~/mynote/memo.org'
-alias sr='screen -xR'
-alias tree='tree --charset XXX'
-alias ycal='cal `date +%Y`'
 alias now='date +%Y%m%d_%H%M%S'
-
+alias sr='screen -xR'
 alias svnco--='svn st | grep ^M | tr -d M | xargs svn revert'
+alias tree='tree --charset XXX'
+alias vimf='vim $(fzf)'
+alias ycal='cal `date +%Y`'
 
 mozc(){
-    if [ $1 = 'dict' ]; then
-        /usr/lib/mozc/mozc_tool --mode=dictionary_tool
-    elif [ $1 = 'word' ]; then
-        /usr/lib/mozc/mozc_tool --mode=word_register_dialog
-    else
-        echo 'mozc [dict|word]'
-    fi
+    case $1 in
+        'dict') /usr/lib/mozc/mozc_tool --mode=dictionary_tool ;;
+        'word') /usr/lib/mozc/mozc_tool --mode=word_register_dialog ;;
+        *) echo 'mozc [dict|word]' ;;
+    esac
 }
 
 alarm(){
@@ -38,25 +35,7 @@ alarm(){
 }
 
 
-alias gita='git add -A . ; git commit -m "ALL Update" ; git push'
 
-cleanse(){
-    # example:
-    #    echo 'アアaaＡ' | cleanse
-    #    => アアAAA
-
-    cat "$1" |
-        sed -s 's/(.*)//g' |
-        sed -s 's/（.*）//g' |
-        sed -s 's/<.*>//g' |
-        sed -s 's/＜.*＞//g' |
-        sed -s 's/\[.*\]//g' |
-        sed -s 's/【.*】//g' |
-        sed -s 's/株式会社//g' |
-        grep -v ダミー  | grep -v ※  |
-        han | nkf -w |
-        tr "[:lower:]" "[:upper:]"
-}
 
 to_dos(){
     iconv -f utf8 -t sjis | perl -pe 's/\n/\r\n/' < /dev/stdin
--- .zsh/20_plugins.zsh
+++ .zsh/20_plugins.zsh
@@ -2,6 +2,7 @@ antigen use oh-my-zsh
 
 antigen bundle b4b4r07/enhancd
 antigen bundle b4b4r07/zsh-gomi
+antigen bundle b4b4r07/cli-finder
 antigen bundle m4i/cdd
 antigen bundle tonyseek/oh-my-zsh-virtualenv-prompt
 antigen bundle zsh-users/zsh-syntax-highlighting
