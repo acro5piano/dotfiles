614948b4e1a9e2c9b4300d82d9deae21e0163f91
gosho-kazuya <ketsume0211@gmail.com>
2016-09-06 09:56:29 +0900

    add new scripts

--- .zsh/50_aliases.zsh
+++ .zsh/50_aliases.zsh
@@ -1,4 +1,5 @@
 alias tmc='tmux loadb -'
 alias rm='echo "This is not the command you are looking for. use trash instead."; false'
 alias tmsplit='tmux source ~/.tmux/scripts/split.tmux'
+alias pcd='cd $(ls | peco)'
 
