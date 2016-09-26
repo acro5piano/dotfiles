d633e6f2a04ccc49acf48923ece98611149180d0
gosho-kazuya <ketsume0211@gmail.com>
2016-09-26 12:23:40 +0900

    add wallpaper

---
+++ .zsh/60_key_binding.sh
@@ -0,0 +1,9 @@
+function backward-kill-word-or-region() {
+    if [ $REGION_ACTIVE -eq 0 ]; then
+        zle backward-kill-word
+    else
+        zle kill-region
+    fi
+}
+zle -N backward-kill-word-or-region
+bindkey "^w" backward-kill-word-or-region
