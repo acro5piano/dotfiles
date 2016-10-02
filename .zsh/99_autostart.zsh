620f498df7ac321ccca738e84a76e7de56c6e507
gosho-kazuya <ketsume0211@gmail.com>
2016-10-02 15:13:13 +0900

    add screensaver

--- .zsh/99_autostart.zsh
+++ .zsh/99_autostart.zsh
@@ -1,4 +1,11 @@
 # Start tmux if x is running and no tmux is running
 [ "$DISPLAY" ] && [ `pgrep -c tmux` -eq 0 ] && tmux
 
-[ "$DISPLAY" ] || startx
+if [ ! "$DISPLAY" ]; then
+    startx
+#    read start_x
+#    case start_x in
+#        'y') start_x ;;
+#        "\n") start_x ;;
+#    esac
+fi
