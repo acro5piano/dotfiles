c7012c3a3d77746ae507fc1033c01f6e78a52805
gosho-kazuya <ketsume0211@gmail.com>
2016-10-05 01:15:55 +0900

    first

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
+
 cd ~
 git clone https://github.com/acro5piano/dotfiles $HOME/.dotfiles
-$HOME/.dotfiles/bin/install_dotfiles
-$HOME/.dotfiles/bin/install_vundle
-vim -c VundleInstall
+$HOME/.dotfiles/bin/dot install
 
