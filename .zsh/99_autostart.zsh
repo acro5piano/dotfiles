fa3029ce413e5c41285adbd45b82584ef82a4999
gosho-kazuya <ketsume0211@gmail.com>
2016-10-04 14:38:57 +0900

    change terminator to xterm

--- .zsh/99_autostart.zsh
+++ .zsh/99_autostart.zsh
@@ -1,10 +1,10 @@
-# Start tmux if x is running and no tmux is running
-[ "$DISPLAY" ] && [ `pgrep -c tmux` -eq 0 ] && tmux
+if [ "$DISPLAY" ]; then
+     # Start tmux if x is running and no tmux is running
+     [ `pgrep -c tmux` -eq 0 ] && tmux
 
-# Start emacs server. Use emacs with `emacsclient`
-[ "$DISPLAY" ] && [ `ps aux | grep  'emacs' | wc -l` -lt 4 ] && emacs --reverse &
-
-if [ ! "$DISPLAY" ]; then
+     # Start emacs server with GUI. Use emacs with `emacsclient`
+     # [ `ps aux | grep  'emacs' | wc -l` -lt 2 ] && nohup emacs --reverse > /dev/null &
+else
     startx
 #    read start_x
 #    case start_x in
@@ -12,3 +12,4 @@ if [ ! "$DISPLAY" ]; then
 #        "\n") start_x ;;
 #    esac
 fi
+
