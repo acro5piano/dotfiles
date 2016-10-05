80c5bded9f8c4b80ea23f1072af662c5f83d164d
gosho-kazuya <gosho-kazuya@919.jp>
2016-10-06 00:31:31 +0900

    to

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -246,10 +246,15 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ; Mozc settings
-(require 'mozc)
-(set-language-environment "Japanese")
-(setq default-input-method "japanese-mozc")
+;;(require 'mozc)
+;;(set-language-environment "Japanese")
+;;(setq default-input-method "japanese-mozc")
+;;
+;;(require 'mozc-popup)
+;;(setq mozc-candidate-style 'popup)
 
-(require 'mozc-popup)
-(setq mozc-candidate-style 'popup)
+(when (require 'skk nil t)
+  (global-set-key (kbd "C-x j") 'skk-auto-fill-mode) ;;良い感じに改行を自動入力してくれる機能
+  (setq default-input-method "japanese-skk")         ;;emacs上での日本語入力にskkをつかう
+  (require 'skk-study))                              ;;変換学習機能の追加
 
