6325fd24b2186b74dfd4e998e1441eb99b1c5c06
gosho-kazuya <gosho-kazuya@919.jp>
2016-10-19 00:44:29 +0900

    delete some zsh plugins

--- .zsh/20_plugins.zsh
+++ .zsh/20_plugins.zsh
@@ -1,14 +1,10 @@
 antigen use oh-my-zsh
 
-#antigen bundle b4b4r07/enhancd
-antigen bundle b4b4r07/zsh-gomi
-antigen bundle b4b4r07/cli-finder
-antigen bundle m4i/cdd
-antigen bundle tonyseek/oh-my-zsh-virtualenv-prompt
-antigen bundle zsh-users/zsh-syntax-highlighting
-antigen bundle zsh-users/zsh-history-substring-search
 antigen bundle acro5piano/exec_on_change
 antigen bundle acro5piano/fd
+#antigen bundle b4b4r07/enhancd
+antigen bundle zsh-users/zsh-syntax-highlighting
+antigen bundle zsh-users/zsh-history-substring-search
 
 # Load the theme.
 antigen theme robbyrussell
