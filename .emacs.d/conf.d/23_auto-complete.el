2d016099b7aa54b571d4e1ef4ac539ac7e6f7b92
gosho-kazuya <gosho-kazuya@919.jp>
2016-10-24 11:32:37 +0900

    php

--- .emacs.d/conf.d/23_auto-complete.el
+++ .emacs.d/conf.d/23_auto-complete.el
@@ -6,13 +6,15 @@
 (global-auto-complete-mode)       ;; これで常にac-modeになる？
 (add-to-list 'ac-modes 'text-mode)         ;; text-modeでも自動的に有効にする
 (add-to-list 'ac-modes 'shell-script-mode)         ;; text-modeでも自動的に有効にする
-(add-to-list 'ac-modes 'fundamental-mode)  ;; fundamental-mode
 (add-to-list 'ac-modes 'org-mode)
 (add-to-list 'ac-modes 'yatex-mode)
 (add-to-list 'ac-modes 'markdown-mode)
 (add-to-list 'ac-modes 'lisp-interaction-mode)
 (add-to-list 'ac-modes 'sql-mode)
 (add-to-list 'ac-modes 'lisp-mode)
+(add-to-list 'ac-modes 'ruby-mode)
+(add-to-list 'ac-modes 'web-mode)
+(add-to-list 'ac-modes 'haml-mode)
 (add-to-list 'ac-modes 'ruby-mode)
 
 (setq ac-use-menu-map t)       ;; 補完メニュー表示時にC-n/C-pで補完候補選択
--- .emacs.d/conf.d/24_twittering-mode.el
+++ .emacs.d/conf.d/24_twittering-mode.el
@@ -2,5 +2,3 @@
 (setq twittering-use-master-password t)
 ;; パスワード暗号ファイル保存先変更 (デフォはホームディレクトリ)
 (setq twittering-private-info-file "~/.emacs.d/twittering-mode.gpg")
-;; 表示する書式 区切り線いれたら見やすい
-(setq twittering-status-format "%i @%s %S %p: n %T  [%@]%r %R %f%Ln -------------------------------------------")
