50e75fb5e8f618a0ed561d7e43096d6ec7a0db75
gosho-kazuya <gosho-kazuya@919.jp>
2016-11-20 23:04:11 +0900

    add git-open

--- .zsh/20_plugins.zsh
+++ .zsh/20_plugins.zsh
@@ -4,7 +4,7 @@ antigen bundle acro5piano/exec_on_change
 antigen bundle acro5piano/fd
 #antigen bundle b4b4r07/enhancd
 antigen bundle zsh-users/zsh-syntax-highlighting
-antigen bundle zsh-users/zsh-history-substring-search
+antigen bundle paulirish/git-open
 
 # Load the theme.
 antigen theme robbyrussell
