591cf55d44734b78d4058faea9e12718c0b8152b
gosho-kazuya <gosho-kazuya@919.jp>
2016-11-28 02:31:50 +0900

    do not start tmux automatically

--- .zsh/99_autostart.zsh
+++ .zsh/99_autostart.zsh
@@ -1,8 +1,8 @@
 if [ "$DISPLAY" ]; then
-     # Start tmux if x is running and no tmux is running
-     [ `pgrep -c tmux` -eq 0 ] && tmux
+    # Start tmux if x is running and no tmux is running
+    # disable because gui emacs is my new standard
+    #[ `pgrep -c tmux` -eq 0 ] && tmux
 
-     # Start emacs server with GUI. Use emacs with `emacsclient`
-     # [ `ps aux | grep  'emacs' | wc -l` -lt 2 ] && nohup emacs --reverse > /dev/null &
+    # Start emacs server with GUI. Use emacs with `emacsclient`
+    # [ `ps aux | grep  'emacs' | wc -l` -lt 2 ] && nohup emacs --reverse > /dev/null &
 fi
-
