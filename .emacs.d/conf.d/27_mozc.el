d401820b77fc812ee3653a9b79f163f2d09fd37f
gosho-kazuya <gosho-kazuya@919.jp>
2016-12-07 07:49:26 +0900

    delete unused settings

--- .emacs.d/conf.d/27_mozc.el
+++ .emacs.d/conf.d/27_mozc.el
@@ -1,12 +1,7 @@
-;; ;; Mozc settings
+;; Mozc settings
 (require 'mozc)
 (set-language-environment "Japanese")
 (setq default-input-method "japanese-mozc")
-
-(add-hook 'helm-mini (lambda()
-                       (mozc-mode -1)))
-(add-hook 'helm-M-x (lambda()
-                      (mozc-mode -1)))
 
 (setq mozc-candidate-style 'overlay)
 
