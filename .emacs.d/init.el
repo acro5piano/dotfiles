0565ad57e83d5db3e6d36fb466fb347f7c69b65e
gosho-kazuya <gosho-kazuya@919.jp>
2016-12-11 04:08:40 +0900

    delete font settings

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -43,12 +43,28 @@
     (git-gutter goto-chg counsel ivy ido-ubiquitous smex ido-vertical-mode helm-swoop rainbow-mode dashboard color-theme-modern color-theme-solarized color-theme chatwork summarye haml-mode lua-mode twittering-mode ace-link yaml-mode magit undo-tree iedit ## browse-kill-ring markdown-mode htmlize cask bind-key auto-complete)))
  '(read-file-name-completion-ignore-case t))
 (put 'downcase-region 'disabled nil)
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#002b36" :foreground "#839496" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 105 :width normal :foundry "Source Code Pro" :family "Source Code Pro"))))
- '(helm-selection ((t (:background "Gray23" :distant-foreground "black")))))
 
-(set-fontset-font t 'japanese-jisx0208 (font-spec :family "IPAExGothic"))
+(custom-set-faces
+ '(default
+    ((t (:inherit nil
+                  :stipple nil
+                  :background "#002b36"
+                  :foreground "#839496"
+                  :inverse-video nil
+                  :box nil
+                  :strike-through nil
+                  :overline nil
+                  :underline nil
+                  :slant normal
+                  :weight normal
+                  :height 105
+                  :width normal
+                  :foundry "Source Code Pro"
+                  :family "Source Code Pro"
+                  ))))
+ '(helm-selection
+   ((t (:background "Gray23"
+                    :distant-foreground "black")))))
+
+(set-fontset-font t 'japanese-jisx0208
+                  (font-spec :family "IPAExGothic"))
