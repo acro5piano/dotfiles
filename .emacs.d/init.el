ec6dd296da4bf6f72026267bc55192e880eb7038
gosho-kazuya <gosho-kazuya@919.jp>
2016-12-25 22:10:07 +0900

    restructure emacs usage

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -79,11 +79,8 @@
 (defun dired-my-append-buffer-name-hint ()
   "Append a auxiliary string to a name of dired buffer."
   (when (eq major-mode 'dired-mode)
-    (let* ((dir (expand-file-name list-buffers-directory))
-           (drive (if (and (eq 'system-type 'windows-nt) ;; Windows の場合はドライブレターを追加
-                           (string-match "^\\([a-zA-Z]:\\)/" dir))
-                      (match-string 1 dir) "")))
-      (rename-buffer (concat (buffer-name) " [" drive "Dired]") t))))
+    (let* ((dir (expand-file-name list-buffers-directory)))
+      (rename-buffer (concat (buffer-name) " [Dired]") t))))
 (add-hook 'dired-mode-hook 'dired-my-append-buffer-name-hint)
 
 
