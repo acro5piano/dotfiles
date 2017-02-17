47dcfc5e7e28c05eed858182dd9e8d3fa3b8652e
gosho-kazuya <gosho-kazuya@919.jp>
2017-02-17 18:24:00 +0900

    add fish settings

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -25,3 +25,15 @@ function g
   end
 end
 
+alias bc='bc -l'
+alias clp='xsel -ob'
+alias cl='xsel -ib'
+alias dc='docker-compose'
+alias grep='grep --color=auto'
+alias la='ls -A'
+alias less='less -R'
+alias ll='ls -al'
+alias l='ls -CF'
+alias ls='ls --color=auto'
+alias now='date +%Y%m%d_%H%M%S'
+alias seishin='cd (mktemp -d)'
