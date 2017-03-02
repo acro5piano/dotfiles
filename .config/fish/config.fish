e72fb5498661b8fc43d0e78f7ff98ec9d7ceb711
gosho-kazuya <gosho-kazuya@919.jp>
2017-03-02 17:45:16 +0900

    fix fish bug?

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -49,17 +49,13 @@ function gup
   end
 end
 
-alias -='cd -'
+# alias -='cd -'
 alias ..='cd ..'
 alias ...='cd ../..'
 alias ....='cd ../../..'
 alias .....='cd ../../../..'
 alias ......='cd ../../../../..'
 alias 1='cd -'
-alias 2='cd -2'
-alias 3='cd -3'
-alias 4='cd -4'
-alias 5='cd -5'
 
 alias bc='bc -l'
 alias clp='xsel -ob'
