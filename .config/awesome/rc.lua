1bd8bf39ed176da45ada315ca42dd7bcaca3e56d
gosho-kazuya <gosho-kazuya@919.jp>
2017-03-09 10:37:06 +0900

    add vi copy mode

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -163,27 +163,12 @@ end
 
 -- {{{ Key bindings
 globalkeys = awful.util.table.join(
-    awful.key({ modkey,           }, "Left",   awful.tag.viewprev       ),
-    awful.key({ modkey,           }, "Right",  awful.tag.viewnext       ),
-    awful.key({ modkey,           }, "Escape", awful.tag.history.restore),
-
-    awful.key({ modkey,           }, "j",
-        function ()
-            awful.client.focus.byidx( 1)
-            if client.focus then client.focus:raise() end
-        end),
-    awful.key({ modkey,           }, "k",
-        function ()
-            awful.client.focus.byidx(-1)
-            if client.focus then client.focus:raise() end
-        end),
-    awful.key({ modkey,           }, "w", function () mymainmenu:show() end),
+    awful.key({ modkey,           }, "p",   awful.tag.viewprev       ),
+    awful.key({ modkey,           }, "n",  awful.tag.viewnext       ),
 
     -- Layout manipulation
-    awful.key({ modkey, "Shift"   }, "j", function () awful.client.swap.byidx(  1)    end),
-    awful.key({ modkey, "Shift"   }, "k", function () awful.client.swap.byidx( -1)    end),
-    awful.key({ modkey, "Control" }, "j", function () awful.screen.focus_relative( 1) end),
-    awful.key({ modkey, "Control" }, "k", function () awful.screen.focus_relative(-1) end),
+    awful.key({ modkey,           }, "j", function () awful.screen.focus_relative( 1) end),
+    awful.key({ modkey,           }, "k", function () awful.screen.focus_relative(-1) end),
     awful.key({ modkey,           }, "u", awful.client.urgent.jumpto),
     awful.key({ modkey,           }, "Tab",
         function ()
@@ -195,29 +180,20 @@ globalkeys = awful.util.table.join(
 
     -- Standard program
     awful.key({ modkey,           }, "b", function () awful.util.spawn("select_window") end),
-    awful.key({ modkey,           }, "p", function () awful.util.spawn("xfce4-screenshooter") end),
-    awful.key({ modkey,           }, "s", function () awful.util.spawn("sudo pm-suspend") end),
+    awful.key({ modkey,           }, "s", function () awful.util.spawn("xfce4-screenshooter") end),
     awful.key({ modkey,           }, "c", function () awful.util.spawn("chromium") end),
-    -- awful.key({ modkey,           }, "b", function () awful.util.spawn("select_window") end),
     awful.key({ modkey,           }, "Return", function () awful.util.spawn(terminal) end),
+
     awful.key({ modkey, "Control" }, "r", awesome.restart),
-    awful.key({ modkey, "Control" }, "l", function () awful.util.spawn("xscreensaver-command -lock") end),
-    awful.key({ modkey, "Shift"   }, "q", awesome.quit),
 
     -- Hardware config
-    awful.key({}, "XF86MonBrightnessUp",      function () awful.util.spawn("xbacklight +5") end),
-    awful.key({}, "XF86MonBrightnessDown",    function () awful.util.spawn("xbacklight -5") end),
+    awful.key({                   }, "XF86MonBrightnessUp",      function () awful.util.spawn("xbacklight +5") end),
+    awful.key({                   }, "XF86MonBrightnessDown",    function () awful.util.spawn("xbacklight -5") end),
+    awful.key({ modkey, "Control" }, "s", function () awful.util.spawn("sudo pm-suspend") end),
+    awful.key({ modkey, "Control" }, "l", function () awful.util.spawn("xscreensaver-command -lock") end),
 
     awful.key({ modkey,           }, "l",     function () awful.tag.incmwfact( 0.05)    end),
     awful.key({ modkey,           }, "h",     function () awful.tag.incmwfact(-0.05)    end),
-    -- awful.key({ modkey, "Shift"   }, "h",     function () awful.tag.incnmaster( 1)      end),
-    -- awful.key({ modkey, "Shift"   }, "l",     function () awful.tag.incnmaster(-1)      end),
-    -- awful.key({ modkey, "Control" }, "h",     function () awful.tag.incncol( 1)         end),
-    -- awful.key({ modkey, "Control" }, "l",     function () awful.tag.incncol(-1)         end),
-    -- awful.key({ modkey,           }, "space", function () awful.layout.inc(layouts,  1) end),
-    -- awful.key({ modkey, "Shift"   }, "space", function () awful.layout.inc(layouts, -1) end),
-
-    -- awful.key({ modkey, "Control" }, "n", awful.client.restore),
 
     -- Prompt
     awful.key({ modkey },            "x",     function () awful.util.spawn("dmenu_run") end)
@@ -225,24 +201,11 @@ globalkeys = awful.util.table.join(
 
 clientkeys = awful.util.table.join(
     awful.key({ modkey,           }, "z",      function (c) c.fullscreen = not c.fullscreen  end),
-    awful.key({ modkey, "Shift"   }, "c",      function (c) c:kill()                         end),
-    awful.key({ modkey, "Control" }, "space",  awful.client.floating.toggle                     ),
+    awful.key({ modkey,           }, "c",      function (c) c:kill()                         end),
+    awful.key({ modkey,           }, "f",  awful.client.floating.toggle                     ),
     awful.key({ modkey, "Control" }, "Return", function (c) c:swap(awful.client.getmaster()) end),
     awful.key({ modkey,           }, "o",      awful.client.movetoscreen                        ),
     awful.key({ modkey,           }, "t",      function (c) c.ontop = not c.ontop            end)
-
-    -- よく間違えるので削除
-    -- awful.key({ modkey,           }, "n",
-    --     function (c)
-    --         The client currently has the input focus, so it cannot be
-    --         minimized, since minimized clients can't have the focus.
-    --         c.minimized = true
-    --     end),
-    -- awful.key({ modkey,           }, "m",
-    --     function (c)
-    --         c.maximized_horizontal = not c.maximized_horizontal
-    --         c.maximized_vertical   = not c.maximized_vertical
-    --     end)
 )
 
 -- Bind all key numbers to tags.
