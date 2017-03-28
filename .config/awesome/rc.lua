f8d0ab3b769cfcf91cb40b4dfc8ae017ab96977d
gosho-kazuya <gosho-kazuya@919.jp>
2017-03-28 11:17:45 +0900

    change guake color theme

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -41,7 +41,7 @@ end
 beautiful.init(awful.util.getdir("config") .. "/themes/default/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "terminator"
+terminal = "guake"
 editor = os.getenv("EDITOR") or "nano"
 editor_cmd = terminal .. " -e " .. editor
 
