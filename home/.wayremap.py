7f957ca75d7ed5789550156f6557eef0edea55fe
Kay Gosho <ketsume0211@gmail.com>
2021-12-10 11:46:37 +0900

    update wayremap config

--- home/.wayremap.py
+++ home/.wayremap.py
@@ -9,6 +9,15 @@ wayremap_config = WayremapConfig(
         'firefoxdeveloperedition',
     ],
     bindings=[
+        # Alternative
+        Binding('alt.k', [[k.KEY_LEFTCTRL, k.KEY_K]]),
+        Binding('alt.h', [[k.KEY_LEFTCTRL, k.KEY_H]]),
+        Binding('ctrl.alt.d', [[k.KEY_LEFTCTRL, k.KEY_D]]),
+        Binding('ctrl.alt.p', [[k.KEY_LEFTCTRL, k.KEY_LEFTSHIFT, k.KEY_A]]),
+
+        # Slack helm!
+        Binding('alt.x', [[k.KEY_LEFTCTRL, k.KEY_K]]),
+
         # Emacs-like key binding
         Binding('ctrl.alt.a', [[k.KEY_LEFTCTRL, k.KEY_HOME]]),
         Binding('ctrl.alt.e', [[k.KEY_LEFTCTRL, k.KEY_END]]),
@@ -17,6 +26,8 @@ wayremap_config = WayremapConfig(
         Binding('ctrl.b', [[k.KEY_LEFT]]),
         Binding('ctrl.p', [[k.KEY_UP]]),
         Binding('ctrl.n', [[k.KEY_DOWN]]),
+        Binding('alt.d', [[k.KEY_LEFTCTRL, k.KEY_DELETE]]),
+        Binding('ctrl.d', [[k.KEY_DELETE]]),
         Binding('ctrl.k',
                 [[k.KEY_LEFTSHIFT, k.KEY_END], [k.KEY_LEFTCTRL, k.KEY_X]]),
         Binding('ctrl.a', [[k.KEY_HOME]]),
@@ -32,10 +43,6 @@ wayremap_config = WayremapConfig(
         Binding('alt.a', [[k.KEY_LEFTCTRL, k.KEY_A]]),
         Binding('alt.c', [[k.KEY_LEFTCTRL, k.KEY_C]]),
         Binding('alt.v', [[k.KEY_LEFTCTRL, k.KEY_V]]),
-        Binding('alt.x', [[k.KEY_LEFTCTRL, k.KEY_X]]),
-
-        # Slack helm!
-        Binding('alt.x', [[k.KEY_LEFTCTRL, k.KEY_K]]),
     ])
 
 run(wayremap_config, '/dev/input/event4')
