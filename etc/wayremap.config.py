42d17ae8b404769957118750c82c316aa82261f1
Kay Gosho <ketsume0211@gmail.com>
2021-12-13 01:28:22 +0900

    update wayremap config version

--- etc/wayremap.config.py
+++ etc/wayremap.config.py
@@ -1,6 +1,7 @@
 from wayremap.config import WayremapConfig, Binding
 from wayremap.main import run
 import uinput as k
+from wayremap import ecodes as e
 
 wayremap_config = WayremapConfig(
     applications=[
@@ -10,41 +11,45 @@ wayremap_config = WayremapConfig(
     ],
     bindings=[
         # Alternative
-        Binding('ctrl.alt.d', [[k.KEY_LEFTCTRL, k.KEY_D]]),
-        Binding('ctrl.alt.p', [[k.KEY_LEFTCTRL, k.KEY_LEFTSHIFT, k.KEY_A]]),
+        Binding([e.KEY_LEFTCTRL, e.KEY_LEFTALT, e.KEY_D],
+                [[k.KEY_LEFTCTRL, k.KEY_D]]),
+        Binding([e.KEY_LEFTCTRL, e.KEY_LEFTALT, e.KEY_P],
+                [[k.KEY_LEFTCTRL, k.KEY_LEFTSHIFT, k.KEY_A]]),
 
         # Slack helm!
-        Binding('alt.x', [[k.KEY_LEFTCTRL, k.KEY_K]]),
+        Binding([e.KEY_LEFTALT, e.KEY_X], [[k.KEY_LEFTCTRL, k.KEY_K]]),
 
         # Emacs-like key binding
-        Binding('ctrl.alt.a', [[k.KEY_LEFTCTRL, k.KEY_HOME]]),
-        Binding('ctrl.alt.e', [[k.KEY_LEFTCTRL, k.KEY_END]]),
-        Binding('ctrl.alt.h', [[k.KEY_LEFTCTRL, k.KEY_BACKSPACE]]),
-        Binding('ctrl.f', [[k.KEY_RIGHT]]),
-        Binding('ctrl.b', [[k.KEY_LEFT]]),
-        Binding('ctrl.p', [[k.KEY_UP]]),
-        Binding('ctrl.n', [[k.KEY_DOWN]]),
-        Binding('alt.d', [[k.KEY_LEFTCTRL, k.KEY_DELETE]]),
-        Binding('ctrl.d', [[k.KEY_DELETE]]),
-        Binding('ctrl.k',
+        Binding([e.KEY_LEFTCTRL, e.KEY_LEFTALT, e.KEY_A],
+                [[k.KEY_LEFTCTRL, k.KEY_HOME]]),
+        Binding([e.KEY_LEFTCTRL, e.KEY_LEFTALT, e.KEY_E],
+                [[k.KEY_LEFTCTRL, k.KEY_END]]),
+        Binding([e.KEY_LEFTCTRL, e.KEY_LEFTALT, e.KEY_H],
+                [[k.KEY_LEFTCTRL, k.KEY_BACKSPACE]]),
+        Binding([e.KEY_LEFTCTRL, e.KEY_F], [[k.KEY_RIGHT]]),
+        Binding([e.KEY_LEFTCTRL, e.KEY_B], [[k.KEY_LEFT]]),
+        Binding([e.KEY_LEFTCTRL, e.KEY_P], [[k.KEY_UP]]),
+        Binding([e.KEY_LEFTCTRL, e.KEY_N], [[k.KEY_DOWN]]),
+        Binding([e.KEY_LEFTALT, e.KEY_D], [[k.KEY_LEFTCTRL, k.KEY_DELETE]]),
+        Binding([e.KEY_LEFTCTRL, e.KEY_D], [[k.KEY_DELETE]]),
+        Binding([e.KEY_LEFTCTRL, e.KEY_K],
                 [[k.KEY_LEFTSHIFT, k.KEY_END], [k.KEY_LEFTCTRL, k.KEY_X]]),
-        Binding('ctrl.a', [[k.KEY_HOME]]),
-        Binding('ctrl.e', [[k.KEY_END]]),
-        Binding('ctrl.y', [[k.KEY_LEFTCTRL, k.KEY_V]]),
-        Binding('alt.f', [[k.KEY_LEFTCTRL, k.KEY_RIGHT]]),
-        Binding('alt.b', [[k.KEY_LEFTCTRL, k.KEY_LEFT]]),
-        Binding('alt.d', [[k.KEY_LEFTCTRL, k.KEY_DELETE]]),
-        Binding('ctrl.h', [[k.KEY_BACKSPACE]]),
-        Binding('ctrl.s', [[k.KEY_LEFTCTRL, k.KEY_F]]),
+        Binding([e.KEY_LEFTCTRL, e.KEY_A], [[k.KEY_HOME]]),
+        Binding([e.KEY_LEFTCTRL, e.KEY_E], [[k.KEY_END]]),
+        Binding([e.KEY_LEFTCTRL, e.KEY_Y], [[k.KEY_LEFTCTRL, k.KEY_V]]),
+        Binding([e.KEY_LEFTALT, e.KEY_F], [[k.KEY_LEFTCTRL, k.KEY_RIGHT]]),
+        Binding([e.KEY_LEFTALT, e.KEY_B], [[k.KEY_LEFTCTRL, k.KEY_LEFT]]),
+        Binding([e.KEY_LEFTCTRL, e.KEY_H], [[k.KEY_BACKSPACE]]),
+        Binding([e.KEY_LEFTCTRL, e.KEY_S], [[k.KEY_LEFTCTRL, k.KEY_F]]),
 
         # OSX-like key binding
-        Binding('alt.a', [[k.KEY_LEFTCTRL, k.KEY_A]]),
-        Binding('alt.c', [[k.KEY_LEFTCTRL, k.KEY_C]]),
-        Binding('alt.v', [[k.KEY_LEFTCTRL, k.KEY_V]]),
+        Binding([e.KEY_LEFTALT, e.KEY_A], [[k.KEY_LEFTCTRL, k.KEY_A]]),
+        Binding([e.KEY_LEFTALT, e.KEY_C], [[k.KEY_LEFTCTRL, k.KEY_C]]),
+        Binding([e.KEY_LEFTALT, e.KEY_V], [[k.KEY_LEFTCTRL, k.KEY_V]]),
 
         # TODO: alt keys are pripritize??
-        Binding('alt.k', [[k.KEY_LEFTCTRL, k.KEY_K]]),
-        Binding('alt.h', [[k.KEY_LEFTCTRL, k.KEY_H]]),
+        Binding([e.KEY_LEFTALT, e.KEY_K], [[k.KEY_LEFTCTRL, k.KEY_K]]),
+        Binding([e.KEY_LEFTALT, e.KEY_H], [[k.KEY_LEFTCTRL, k.KEY_H]]),
     ])
 
 run(wayremap_config, '/dev/input/event4')
