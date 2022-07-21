752bcbc2e8424827189705248cdfcf89b51f056f
Kay Gosho <ketsume0211@gmail.com>
2022-07-21 15:04:20 +0900

    slim format buffer defs

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -53,39 +53,17 @@ vim.api.nvim_create_autocmd({ "BufReadPost" }, {
 	end,
 })
 
-vim.api.nvim_create_autocmd({ "BufWritePre" }, {
-	pattern = { "*.lua" },
-	callback = require("nvim-format-buffer").create_format_fn("stylua -"), -- requires `sudo pacman -S stylua`
-})
-
-vim.api.nvim_create_autocmd({ "BufWritePre" }, {
-	pattern = { "*.py" },
-	callback = require("nvim-format-buffer").create_format_fn("black -q - | isort -"), -- requires `pip install black isort`
-})
-
-vim.api.nvim_create_autocmd({ "BufWritePre" }, {
-	pattern = { "*.js", "*.jsx", "*.ts", "*.tsx" },
-	callback = require("nvim-format-buffer").create_format_fn("prettier --parser typescript 2>/dev/null"),
-})
-
-vim.api.nvim_create_autocmd({ "BufWritePre" }, {
-	pattern = { "*.md" },
-	callback = require("nvim-format-buffer").create_format_fn("prettier --parser markdown 2>/dev/null"),
-})
-
-vim.api.nvim_create_autocmd({ "BufWritePre" }, {
-	pattern = { "*.css" },
-	callback = require("nvim-format-buffer").create_format_fn("prettier --parser css"),
-})
-
-vim.api.nvim_create_autocmd({ "BufWritePre" }, {
-	pattern = { "*.rs" },
-	callback = require("nvim-format-buffer").create_format_fn("rustfmt --edition 2021"),
-})
-
-vim.api.nvim_create_autocmd({ "BufWritePre" }, {
-	pattern = { "*.sql" },
-	callback = require("nvim-format-buffer").create_format_fn("sql-formatter --config ~/sql-formatter.json"), -- requires `npm -g i sql-formatter`
+require("nvim-format-buffer").setup({
+	format_rules = {
+		{ pattern = { "*.lua" }, command = "stylua -" },
+		{ pattern = { "*.py" }, command = "black -q - | isort -" },
+		{ pattern = { "*.js", "*.jsx", "*.ts", "*.tsx" }, command = "prettier --parser typescript 2>/dev/null" },
+		{ pattern = { "*.md" }, command = "prettier --parser markdown 2>/dev/null" },
+		{ pattern = { "*.md" }, command = "prettier --parser markdown 2>/dev/null" },
+		{ pattern = { "*.css" }, command = "prettier --parser css" },
+		{ pattern = { "*.rs" }, command = "rustfmt --edition 2021" },
+		{ pattern = { "*.sql" }, command = "sql-formatter --config ~/sql-formatter.json" }, -- requires `npm -g i sql-formatte`
+	},
 })
 
 local fzf_lua = require("fzf-lua")
