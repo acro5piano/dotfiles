00309816ae8ad6d7c19ceabdbec65e8a2bfca714
Kay Gosho <ketsume0211@gmail.com>
2022-09-07 17:06:29 +0900

    add content type

--- home/bin/xsel-serve.mjs
+++ home/bin/xsel-serve.mjs
@@ -6,7 +6,6 @@ import { networkInterfaces } from 'os'
 
 const nic = process.argv[2] || 'wlan0'
 
-
 const log = (t, level = 'INFO') =>
   console.log(
     `[${new Date().toLocaleTimeString()}] [${level}] ` + JSON.stringify(t),
@@ -22,6 +21,7 @@ createServer((req, res) => {
     }
     const pasted = String(stdout).trim()
     log({ pasted })
+    res.setHeader('Content-Type', 'text/plain; charset=utf-8')
     res.end(pasted)
   })
 }).listen(2000, '0.0.0.0', () => {
