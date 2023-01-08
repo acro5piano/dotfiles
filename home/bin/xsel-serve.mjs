b7907b1aa6cd98ba0aa5908c5859791419601be8
Kay Gosho <ketsume0211@gmail.com>
2023-01-08 18:37:47 +0900

    enable to paste clipboard text via button

--- home/bin/xsel-serve.mjs
+++ home/bin/xsel-serve.mjs
@@ -21,11 +21,64 @@ createServer((req, res) => {
     }
     const pasted = String(stdout).trim()
     log({ pasted })
-    res.setHeader('Content-Type', 'text/plain; charset=utf-8')
-    res.end(pasted)
+    res.setHeader('Content-Type', 'text/html; charset=utf-8')
+    res.end(render(pasted))
   })
 }).listen(2000, '0.0.0.0', () => {
   const nets = networkInterfaces()
   log({ msg: `Please Visit http://${nets[`${nic}`][0]['address']}:2000` })
   log({ msg: 'listening on :2000' })
 })
+
+const html = String.raw
+
+/**
+ * @param {string} content - the content to be pasted
+ */
+const render = (content) => html`
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <meta charset="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <title>Clipboard</title>
+      <style>
+        .button {
+          position: fixed;
+          bottom: 0;
+          width: 100%;
+        }
+        button {
+          font-size: 2rem;
+          padding: 0.5rem 1rem;
+          border: 1px solid #ccc;
+          border-radius: 0.5rem;
+          background: #eee;
+          cursor: pointer;
+          width: 88%;
+          margin: 2rem auto;
+          display: block;
+        }
+        textarea {
+          width: 100%;
+          height: 300px;
+        }
+      </style>
+      <script>
+        function onClick() {
+          const textarea = document.querySelector('textarea')
+          textarea.select()
+          document.execCommand('copy')
+          textarea.blur()
+        }
+      </script>
+    </head>
+    <body>
+      <!-- prettier-ignore -->
+      <textarea id="content">${content}</textarea>
+      <div class="button">
+        <button onclick="onClick()">Copy</button>
+      </div>
+    </body>
+  </html>
+`
