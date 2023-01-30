5c23639dfaea9a2341e18ea784fa8014b54c2872
Kay Gosho <ketsume0211@gmail.com>
2023-01-30 23:17:51 +0900

    do not use bluetooth as the keyboard makes mistypes

--- usr/lib/systemd/system-sleep/around-suspend.sh
+++ usr/lib/systemd/system-sleep/around-suspend.sh
@@ -3,14 +3,14 @@
 set -eux
 
 if [ $1 == "pre" ]; then
-    bluetoothctl power off
+    # bluetoothctl power off
     light -O
 fi
 
 if [ $1 == "post" ]; then
     light -I
     systemctl restart iwd
-    bluetoothctl power on
-    bluetoothctl pairable on
-    bluetoothctl scan on
+    # bluetoothctl power on
+    # bluetoothctl pairable on
+    # bluetoothctl scan on
 fi
