486a0f1a2f0564106e001965d36c614281369bc8
Kay Gosho <ketsume0211@gmail.com>
2023-02-19 17:03:20 +0900

    replace sumneko with lua_ls

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -342,11 +342,20 @@ lsp.solargraph.setup({
 		},
 	},
 })
-lsp.sumneko_lua.setup({
+require("lspconfig").lua_ls.setup({
 	settings = {
 		Lua = {
+			runtime = {
+				-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+				version = "LuaJIT",
+			},
 			diagnostics = {
-				globals = { "vim", "describe" },
+				-- Get the language server to recognize the `vim` global
+				globals = { "vim" },
+			},
+			workspace = {
+				-- Make the server aware of Neovim runtime files
+				library = vim.api.nvim_get_runtime_file("", true),
 			},
 		},
 	},
