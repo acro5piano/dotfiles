bda472fc68a2a5ae9304789e9ce0a6ba75f4be04
Kay Gosho <ketsume0211@gmail.com>
2023-03-09 00:22:44 +0900

    fix: add delay to support bluetooth keyboard (ble micro pro

--- home/bin/xset-auto.py
+++ home/bin/xset-auto.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import subprocess
+import time
 
 import pyudev
 
@@ -8,14 +9,17 @@ import pyudev
 def main():
     context = pyudev.Context()
     monitor = pyudev.Monitor.from_netlink(context)
-    monitor.filter_by(subsystem="usb")
+    # monitor.filter_by(subsystem="usb")
     monitor.start()
 
     for device in iter(monitor.poll, None):
 
         dt = device.get("DEVTYPE")
 
-        if dt == "usb_device":
+        if dt == "usb_device" or dt == "link":
+            if dt == "link":
+                # Bluetooth device requires this delay
+                time.sleep(2)
             subprocess.call(["xset", "r", "rate", "220", "60"])
             print("Keyboard rate set to 220, 60")
 
