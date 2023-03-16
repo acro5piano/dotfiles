e0ce65e3815f49b4c01fcce8cd1265923398f946
Kay Gosho <ketsume0211@gmail.com>
2023-03-16 17:02:36 +0900

    fix: do not restart bluetooth on sleep

--- usr/lib/systemd/system-sleep/around-suspend.sh
+++ usr/lib/systemd/system-sleep/around-suspend.sh
@@ -9,8 +9,8 @@ fi
 if [ $1 == "post" ]; then
     light -I
     systemctl restart iwd
-    systemctl restart bluetooth
-    bluetoothctl power on
-    bluetoothctl pairable on
-    bluetoothctl scan on
+    # systemctl restart bluetooth
+    # bluetoothctl power on
+    # bluetoothctl pairable on
+    # bluetoothctl scan on
 fi
