9acab64487bf281b4dd97e14d668282f48ff4468
Kay Gosho <ketsume0211@gmail.com>
2023-06-26 19:23:32 +0900

    fix: use super+p to launch a program, for ergonomics

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -156,6 +156,7 @@ require("nvim-format-buffer").setup({
     },
     { pattern = { "*.md" }, command = prettier_bin() .. " --parser markdown 2>/dev/null | perl -pe 's/\\t/  /g'" },
     { pattern = { "*.css" }, command = prettier_bin() .. " --parser css" },
+    { pattern = { "*.html" }, command = prettier_bin() .. " --parser html" },
     { pattern = { "*.graphql" }, command = prettier_bin() .. " --parser graphql" },
     { pattern = { "*.rs" }, command = "rustfmt --edition 2021" },
     { pattern = { "*.sql" }, command = "sql-formatter --config ~/sql-formatter.json" }, -- requires `npm -g i sql-formatter`
