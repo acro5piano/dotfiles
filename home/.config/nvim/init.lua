043d7a32d5f301dcf6c2a2c8babb48f041d98370
Kay Gosho <ketsume0211@gmail.com>
2023-08-26 20:01:35 +0900

    fix: remove autotag, as it is buggy!!

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -578,7 +578,7 @@ require("nvim-treesitter.configs").setup({
     end,
   },
   indent = { enable = true },
-  autotag = { enable = true },
+  autotag = { enable = false },
   textobjects = {
     select = {
       enable = true,
@@ -600,7 +600,7 @@ require("nvim-treesitter.configs").setup({
   },
 })
 
-require("nvim-ts-autotag").setup()
+-- require("nvim-ts-autotag").setup()
 
 -- used in snippet
 UpperFirstLetter = my_util.upper_first_letter
