43293520f711579adc3f1497a57ad91504d2aba6
Kay Gosho <ketsume0211@gmail.com>
2026-07-19 18:01:33 +0900

    feat: create simple video cutter

---
+++ home/bin/video-cutter.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -euo pipefail
+
+file=$1
+start=$2
+end=$3
+
+output="${file%.*}-sliced.mp4"
+
+ffmpeg \
+  -ss "$start" \
+  -to "$end" \
+  -i "$file" \
+  -y \
+  -vf "scale=1280:720:force_original_aspect_ratio=decrease:force_divisible_by=2" \
+  -c:v libx264 \
+  -preset veryfast \
+  -crf 27 \
+  -c:a aac \
+  -b:a 96k \
+  -movflags +faststart \
+  "$output"
